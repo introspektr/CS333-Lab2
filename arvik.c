4afc5923cf7a1da2e483dea5a4a35973d3e5a016
Cody Jackson <codja@pdx.edu>
2025-02-12 20:53:54 -0800

    Lab 2 Makefile Commit

--- arvik.c
+++ arvik.c
@@ -12,6 +12,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <string.h>
+#include <time.h>
 
 typedef struct stat stat_t;
 
@@ -32,7 +33,10 @@ typedef struct stat stat_t;
 #endif // ON 
 
 void showHelp(void);
+void printTOC(int, int);
+void getPermString(mode_t, char*);
 void writeFileToArchive(int, int, int, const char*);
+void extractArchive(int, int);            
 
 int main(int argc, char* argv[]){
     int opt = -1;
@@ -150,6 +154,14 @@ int main(int argc, char* argv[]){
                 close(fd);
                 exit(BAD_TAG);
             }
+
+            if (action == ACTION_TOC){
+                printTOC(fd, verboseFlag);
+            }
+
+            if (action == ACTION_EXTRACT){
+                extractArchive(fd, verboseFlag);            
+            }
         }
     }
     close(fd);
@@ -170,9 +182,127 @@ void showHelp(void){
            "\t-h           show help text\n");
 }
 
-void writeFileToArchive(int iarch, int member_fd, int determMode, const char* fileName){
+void printTOC(int iarch, int verboseFlag){
+    ar_hdr_t md;
+    char buffer[BUF_SIZE] = {'\0'};
+    char dateBuf[20];
+    char permissions[10];
+    char* back_pos = NULL;
+    mode_t mode;
+    time_t mtime;
+    struct tm *timeinfo;
+    off_t fileSize;
+
+    while (read(iarch, &md, sizeof(ar_hdr_t)) == sizeof(ar_hdr_t)){
+        memset(buffer, 0, sizeof(buffer));
+        strncpy(buffer, md.ar_name, sizeof(md.ar_name) - 1);
+        buffer[sizeof(md.ar_name) - 1] = '\0';
+
+        if ((back_pos = strchr(buffer, '/')) != NULL){
+            *back_pos = '\0';
+        }
+
+        if (verboseFlag){ 
+            mode = (mode_t) strtol(md.ar_mode, NULL, 8);
+            getPermString(mode, permissions);
+
+            mtime = strtol(md.ar_date, NULL, 10);
+            timeinfo = localtime(&mtime);
+            strftime(dateBuf, sizeof(dateBuf), "%b %e %R %Y", timeinfo);
+
+            printf("%s %ld/%ld %ld %s %s\n", permissions,
+                   strtol(md.ar_uid, NULL, 10),
+                   strtol(md.ar_gid, NULL, 10),
+                   strtol(md.ar_size, NULL, 10),
+                   dateBuf, buffer);
+        }
+        else{
+            printf("%s\n", buffer);
+        }
+
+        fileSize = strtol(md.ar_size, NULL, 10);
+        lseek(iarch, fileSize, SEEK_CUR);
+
+        if (fileSize % 2 != 0){
+            lseek(iarch, 1, SEEK_CUR);
+        }
+    }
+}
+
+void getPermString(mode_t mode, char* permissions){
+    // Set user permissions
+    if ((mode & S_IRUSR) != 0){
+        permissions[0] = 'r';
+    }
+    else{
+        permissions[0] = '-';
+    }
+
+    if ((mode & S_IWUSR) != 0){
+        permissions[1] = 'w';
+    }
+    else{
+        permissions[1] = '-';
+    }
+
+    if ((mode & S_IXUSR) != 0){
+        permissions[2] = 'x';
+    }
+    else{
+        permissions[2] = '-';
+    }
+
+    // Set group permissions
+    if ((mode & S_IRGRP) != 0){
+        permissions[3] = 'r';
+    }
+    else{
+        permissions[3] = '-';
+    }
+
+    if ((mode & S_IWGRP) != 0){
+        permissions[4] = 'w';
+    }
+    else{
+        permissions[4] = '-';
+    }
+
+    if ((mode & S_IXGRP) != 0){
+        permissions[5] = 'x';
+    }
+    else{
+        permissions[5] = '-';
+    }
+
+    // Set other (world) permissions
+    if ((mode & S_IROTH) != 0){
+        permissions[6] = 'r';
+    }
+    else{
+        permissions[6] = '-';
+    }
+
+    if ((mode & S_IWOTH) != 0){
+        permissions[7] = 'w';
+    }
+    else{
+        permissions[7] = '-';
+    }
+
+    if ((mode & S_IXOTH) != 0){
+        permissions[8] = 'x';
+    }
+    else{
+        permissions[8] = '-';
+    }
+
+    // Null terminate the string
+    permissions[9] = '\0';
+}
+
+void writeFileToArchive(int iarch, int member_fd, int determFlag, const char* fileName){
     stat_t fileMetaData;
-    ar_hdr_t header;
+    ar_hdr_t md;
     ssize_t bytesRead;
     ssize_t bytesWritten;
     size_t nameLen;
@@ -189,7 +319,7 @@ void writeFileToArchive(int iarch, int member_fd, int determMode, const char* fi
         exit(CREATE_FAIL);
     }
 
-    if (determMode == ON){
+    if (determFlag == ON){
         fileMetaData.st_mtime = 0;
         fileMetaData.st_uid = 0;
         fileMetaData.st_gid = 0;
@@ -197,48 +327,48 @@ void writeFileToArchive(int iarch, int member_fd, int determMode, const char* fi
     }
     
     // Fill the header struct with file metadata
-    
+
     // File name (16 bytes)
-    strncpy(header.ar_name, fileName, sizeof(header.ar_name) - 1);
-    header.ar_name[sizeof(header.ar_name) - 1] = '\0'; // Ensure null termination
-    nameLen = strlen(header.ar_name);
+    strncpy(md.ar_name, fileName, sizeof(md.ar_name) - 1);
+    md.ar_name[sizeof(md.ar_name) - 1] = '\0'; // Ensure null termination
+    nameLen = strlen(md.ar_name);
 
     //Append '/' and pad with spaces
-    if (nameLen < sizeof(header.ar_name) - 1){
-        header.ar_name[nameLen] = '/'; // Append '/'
-        memset(header.ar_name + nameLen + 1, ' ', sizeof(header.ar_name) - nameLen - 1);
+    if (nameLen < sizeof(md.ar_name) - 1){
+        md.ar_name[nameLen] = '/'; // Append '/'
+        memset(md.ar_name + nameLen + 1, ' ', sizeof(md.ar_name) - nameLen - 1);
     }
     else{
-        header.ar_name[sizeof(header.ar_name) - 2] = '/';
-        header.ar_name[sizeof(header.ar_name) - 1] = ' ';
+        md.ar_name[sizeof(md.ar_name) - 2] = '/';
+        md.ar_name[sizeof(md.ar_name) - 1] = ' ';
     }
 
     // Time info (12 bytes)
     sprintf(dateBuf, "%-12ld", (long) fileMetaData.st_mtime);
-    memcpy(header.ar_date, dateBuf, sizeof(header.ar_date));
+    memcpy(md.ar_date, dateBuf, sizeof(md.ar_date));
            
     // User ID (6 bytes)
     sprintf(uidBuf, "%-6d", fileMetaData.st_uid);
-    memcpy(header.ar_uid, uidBuf, sizeof(header.ar_uid));
+    memcpy(md.ar_uid, uidBuf, sizeof(md.ar_uid));
 
     // Group ID (6 bytes)
     sprintf(gidBuf, "%-6d", fileMetaData.st_gid);
-    memcpy(header.ar_gid, gidBuf, sizeof(header.ar_gid));
+    memcpy(md.ar_gid, gidBuf, sizeof(md.ar_gid));
 
     //File mode (8 bytes)
     sprintf(modeBuf, "%-8o", fileMetaData.st_mode);
-    memcpy(header.ar_mode, modeBuf, sizeof(header.ar_mode));
+    memcpy(md.ar_mode, modeBuf, sizeof(md.ar_mode));
 
     //File size (10 bytes)
     sprintf(sizeBuf, "%-10ld", (long) fileMetaData.st_size);
-    memcpy(header.ar_size, sizeBuf, sizeof(header.ar_size));
+    memcpy(md.ar_size, sizeBuf, sizeof(md.ar_size));
 
     //Magic number (2 bytes)
-    memcpy(header.ar_fmag, ARFMAG, sizeof(header.ar_fmag));
+    memcpy(md.ar_fmag, ARFMAG, sizeof(md.ar_fmag));
 
     // Write the header to the archive
-    bytesWritten = write(iarch, &header, sizeof(header));
-    if (bytesWritten != sizeof(header)) {
+    bytesWritten = write(iarch, &md, sizeof(ar_hdr_t));
+    if (bytesWritten != sizeof(md)) {
         perror("Error writing file header to archive");
         exit(CREATE_FAIL);
     }
@@ -258,6 +388,80 @@ void writeFileToArchive(int iarch, int member_fd, int determMode, const char* fi
         if (write(iarch, &pad, 1) != 1) {
             perror("Error padding file data");
             exit(CREATE_FAIL);
+        }
+    }
+}
+
+void extractArchive(int iarch, int verboseFlag){
+    ar_hdr_t md;
+    char* back_pos;
+    int output_fd;
+    char buffer[BUF_SIZE];
+    ssize_t bytesRead;
+    ssize_t bytesWritten;
+    off_t fileSize;
+    mode_t fileMode;
+    struct timespec times[2];
+
+    while (read(iarch, &md, sizeof(ar_hdr_t)) > 0){
+        char fileName[sizeof(md.ar_name) + 1];
+        strncpy(fileName, md.ar_name, sizeof(fileName));
+        fileName[sizeof(md.ar_name)] = '\0';
+        back_pos = strchr(fileName, '/');
+        if (back_pos != NULL){
+            *back_pos = '\0';
+        }
+
+        output_fd = open(fileName, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+        if (output_fd == -1){
+            perror("Error opening output file");
+            close(iarch);
+            exit(EXTRACT_FAIL);
+        }
+
+        fileSize = atol(md.ar_size);
+        while (fileSize > 0){
+            bytesRead = read(iarch, buffer, MIN(sizeof(buffer), (size_t)fileSize));
+            if (bytesRead <= 0){
+                perror("Error reading archive data");
+                close(output_fd);
+                close(iarch);
+                exit(EXTRACT_FAIL);
+            }
+            bytesWritten = write(output_fd, buffer, bytesRead);
+            if (bytesWritten != bytesRead){
+                perror("Error writing output file");
+                close(output_fd);
+                close(iarch);
+                exit(EXTRACT_FAIL);
+            }
+            fileSize -= bytesRead;
+        }
+
+        fileMode = strtol(md.ar_mode, NULL, 8);
+
+        if (fchmod(output_fd, fileMode) == 1){
+            perror("Error setting file permissions");
+            close(output_fd);
+            close(iarch);
+            exit(EXTRACT_FAIL);
+        }
+
+        times[0].tv_sec = strtol(md.ar_date, NULL, 10);
+        times[1].tv_sec = strtol(md.ar_date, NULL, 10);
+        if (futimens(output_fd, times) == -1){
+            perror("Error setting file timestamps");
+            close(output_fd);
+            close(iarch);
+            exit(EXTRACT_FAIL);
+        }
+
+        if (verboseFlag){
+            printf("x - %s\n", fileName);
+        }
+
+        if (atoi(md.ar_size) % 2 != 0){
+            lseek(iarch, 1, SEEK_CUR);
         }
     }
 }
